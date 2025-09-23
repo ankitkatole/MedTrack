96f74ba6a2887546a9368ca1bb8c62f638c0ec7f
ankitkatole <katoleankit06@gmail.com>
2025-09-23 20:48:30 +0530

    Detailed

--- server/src/routes/pharmacist.js
+++ server/src/routes/pharmacist.js
@@ -14,5 +14,8 @@ router.get('/prescriptions/patient/:medTrackId', auth(), getPrescriptionsByPatie
 router.get('/prescriptions/dispensed', auth(), listPharmacistDispensedPrescriptions);
 router.get('/prescriptions/:id', auth(), getPrescriptionByIdAsPharmacist);
 router.post('/dispense/:id', auth(), dispensePrescription);
+// Express route
+router.get('/pharmacy/prescriptions/dispensed', auth, individualDescription);
+
 
 module.exports = router;
